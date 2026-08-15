e8b371aa54e3f8331c99333b45fef7119c2ea9b3
hanenashi <12799117+hanenashi@users.noreply.github.com>
2026-08-16 03:11:07 +0900

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "net.hanenashi.tilezz"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     lint {
